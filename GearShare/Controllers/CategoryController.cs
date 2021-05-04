e840c38d80f76042fa1af44fbdfbab6e33439806
cslunetta <cslunetta@outlook.com>
2021-05-04 12:57:32 -0500

    added category repo and controller

---
+++ GearShare/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using GearShare.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GearShare.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var categories = _categoryRepository.GetAllCategories();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var category = _categoryRepository.GetCategoryById(id);
+            return Ok(category);
+        }
+    }
+}
--- GearShare/Repositories/CategoryRepository.cs
+++ GearShare/Repositories/CategoryRepository.cs
@@ -1,4 +1,6 @@
 ﻿using GearShare.Models;
+using GearShare.Utils;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
@@ -13,12 +15,65 @@ namespace GearShare.Repositories
 
         public List<Category> GetAllCategories()
         {
-            throw new NotImplementedException();
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT  Id, [Name]
+                          FROM  Category";
+
+                    var reader = cmd.ExecuteReader();
+                    var categories = new List<Category>();
+
+                    while (reader.Read())
+                    {
+                        categories.Add(NewCategoryFromReader(reader));
+                    }
+
+                    reader.Close();
+
+                    return categories;
+                }
+            }
         }
 
         public Category GetCategoryById(int id)
         {
-            throw new NotImplementedException();
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT  Id, [Name]
+                          FROM  Category
+                         WHERE  Id = @Id";
+
+                    DbUtils.AddParameter(cmd, "@Id", id);
+
+                    var reader = cmd.ExecuteReader();
+                    Category category = null;
+
+                    if (reader.Read())
+                    {
+                        category = NewCategoryFromReader(reader);
+                    }
+
+                    reader.Close();
+                    return category;
+                }
+            }
+        }
+
+        private Category NewCategoryFromReader(SqlDataReader reader)
+        {
+            return new Category()
+            {
+                Id = DbUtils.GetInt(reader, "Id"),
+                Name = DbUtils.GetString(reader, "Name")
+            };
         }
     }
 }
